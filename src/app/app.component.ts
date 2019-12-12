1fe96f44494be0509feb6e3106c09cd42ce4164d
Vladyslav Ishchenko <shoumonai.neko@gmail.com>
2019-12-12 13:47:59 +0200

    slomalos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store'
-import { AppState } from './model/app-state.model'
-import { Observable } from 'rxjs';
-import { todoItem } from './model/todo.model';
-import { AddTodoItemAction } from './store/actions/todo.actions';
-import { v4 as uuid } from 'uuid'
+
 
 
 @Component({
@@ -12,20 +7,6 @@ import { v4 as uuid } from 'uuid'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private store: Store<AppState>){}
-  todoItems$: Observable<Array<todoItem>>
-  newTodoItem: todoItem = {id: '', text: '', isDone: false}
-
-  ngOnInit(): void {
-   this.todoItems$ = this.store.select(store => store.todoList);
-   setTimeout(() => {
-    this.addItem()
-   }, 2000)
-  }
-  addItem(){
-    this.newTodoItem.id = uuid();
-    this.store.dispatch(new AddTodoItemAction(this.newTodoItem));
-    this.newTodoItem = {id: '', text: '', isDone: false}
-  }
+export class AppComponent{
+  
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,7 +1,12 @@
 import { Component,  OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TodoService } from '../shared/todos.service';
 import { AppComponent } from '../app.component'
-
+import { Store } from '@ngrx/store'
+import { AppState } from '../model/app-state.model'
+import { Observable } from 'rxjs';
+import { todoItem } from '../model/todo.model';
+import { AddTodoItemAction } from '../store/actions/todo.actions';
+import { v4 as uuid } from 'uuid'
 
 @Component({
   selector: 'app-todos',
@@ -9,14 +14,23 @@ import { AppComponent } from '../app.component'
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+  
 
   private loading: boolean = true;
 
-  constructor(public appComponent: AppComponent) { }
+  constructor(private store: Store<AppState>){}
+  todoItems$: Observable<Array<todoItem>>
+  newTodoItem: todoItem = {id: '', text: '', isDone: false}
 
-  ngOnInit() {
-    
+  ngOnInit(): void {
+   this.todoItems$ = this.store.select(store => store.todoList);
   }
+  addItem(){
+    this.newTodoItem.id = uuid();
+    this.store.dispatch(new AddTodoItemAction(this.newTodoItem));
+    this.newTodoItem = {id: '', text: '', isDone: false}
+  }
+  
 /*
   onChange(id: number){
     this.todosService.onToggle(id)
